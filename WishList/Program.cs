2ef5ee0eb8041e1509b5646ab9dd2d4befa7fe38
alambrow <68347960+alambrow@users.noreply.github.com>
2021-10-18 16:48:46 -0500

    exercise five

---
+++ WishList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+System.Console.WriteLine("My wish list: \n");
+
+List<Wish> GetWishes()
+{
+    List<Wish> wishes = new List<Wish> {
+        new Wish {
+            Label = "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'"
+        },
+        new Wish {
+            Label = "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'"
+        },
+        new Wish {
+            Label = "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'"
+        }
+    };
+    return wishes;
+}
+
+List<Wish> myWishes = GetWishes();
+
+foreach(Wish myWish in myWishes)
+{
+    System.Console.WriteLine(myWish.Label);
+}
+
+public class Wish
+{
+    public string Label { get; set; }
+}
